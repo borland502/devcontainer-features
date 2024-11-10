5296fc1198167bdbc9b4a2e8f4bd63612e0ad51e
HDPC1_qnet <jeremy.hettenhouser@icf.com>
2024-11-10 12:50:13 -0500

    Common, node, & python will all succeed if run together

--- src/common-dev/lib/library_scripts.sh
+++ src/common-dev/lib/library_scripts.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -i
 
-
 clean_download() {
     # The purpose of this function is to download a file with minimal impact on container layer size
-    # this means if no valid downloader is found (curl or wget) then we install a downloader (currently wget) in a 
-    # temporary manner, and making sure to 
+    # this means if no valid downloader is found (curl or wget) then we install a downloader (currently wget) in a
+    # temporary manner, and making sure to
     # 1. uninstall the downloader at the return of the function
     # 2. revert back any changes to the package installer database/cache (for example apt-get lists)
-    # The above steps will minimize the leftovers being created while installing the downloader 
+    # The above steps will minimize the leftovers being created while installing the downloader
     # Supported distros:
     #  debian/ubuntu/alpine
-    
+
     url=$1
     output_location=$2
     tempdir=$(mktemp -d)
@@ -19,8 +18,8 @@ clean_download() {
     function _apt_get_install() {
         tempdir=$1
 
-        # copy current state of apt list - in order to revert back later (minimize contianer layer size) 
-        cp -p -R /var/lib/apt/lists $tempdir 
+        # copy current state of apt list - in order to revert back later (minimize contianer layer size)
+        cp -p -R /var/lib/apt/lists $tempdir
         apt-get update -y
         apt-get -y install --no-install-recommends wget ca-certificates
     }
@@ -38,17 +37,17 @@ clean_download() {
 
     function _apk_install() {
         tempdir=$1
-        # copy current state of apk cache - in order to revert back later (minimize contianer layer size) 
-        cp -p -R /var/cache/apk $tempdir 
+        # copy current state of apk cache - in order to revert back later (minimize contianer layer size)
+        cp -p -R /var/cache/apk $tempdir
 
-        apk add --no-cache  wget
+        apk add --no-cache wget
     }
 
     function _apk_cleanup() {
         tempdir=$1
 
         echo "removing wget"
-        apk del wget 
+        apk del wget
     }
     # try to use either wget or curl if one of them already installer
     if type curl >/dev/null 2>&1; then
@@ -60,10 +59,10 @@ clean_download() {
     fi
 
     # in case none of them is installed, install wget temporarly
-    if [ -z $downloader ] ; then
-        if [ -x "/usr/bin/apt-get" ] ; then
+    if [ -z $downloader ]; then
+        if [ -x "/usr/bin/apt-get" ]; then
             _apt_get_install $tempdir
-        elif [ -x "/sbin/apk" ] ; then
+        elif [ -x "/sbin/apk" ]; then
             _apk_install $tempdir
         else
             echo "distro not supported"
@@ -73,27 +72,26 @@ clean_download() {
         downloader_installed="true"
     fi
 
-    if [ $downloader = "wget" ] ; then
+    if [ $downloader = "wget" ]; then
         wget -q $url -O $output_location
     else
-        curl -sfL $url -o $output_location 
+        curl -sfL $url -o $output_location
     fi
 
     # NOTE: the cleanup procedure was not implemented using `trap X RETURN` only because
     # alpine lack bash, and RETURN is not a valid signal under sh shell
-    if ! [ -z $downloader_installed  ] ; then
-        if [ -x "/usr/bin/apt-get" ] ; then
+    if ! [ -z $downloader_installed ]; then
+        if [ -x "/usr/bin/apt-get" ]; then
             _apt_get_cleanup $tempdir
-        elif [ -x "/sbin/apk" ] ; then
+        elif [ -x "/sbin/apk" ]; then
             _apk_cleanup $tempdir
         else
             echo "distro not supported"
             exit 1
         fi
-    fi 
+    fi
 
 }
-
 
 ensure_nanolayer() {
     # Ensure existance of the nanolayer cli program
@@ -114,7 +112,7 @@ ensure_nanolayer() {
                 echo "Found a pre-existing nanolayer in PATH"
                 nanolayer_location=nanolayer
             fi
-        elif [ -f "${NANOLAYER_CLI_LOCATION}" ] && [ -x "${NANOLAYER_CLI_LOCATION}" ] ; then
+        elif [ -f "${NANOLAYER_CLI_LOCATION}" ] && [ -x "${NANOLAYER_CLI_LOCATION}" ]; then
             nanolayer_location=${NANOLAYER_CLI_LOCATION}
             echo "Found a pre-existing nanolayer which were given in env variable: $nanolayer_location"
         fi
@@ -135,21 +133,20 @@ ensure_nanolayer() {
 
     fi
 
-    # If not previuse installation found, download it temporarly and delete at the end of the script 
+    # If not previuse installation found, download it temporarly and delete at the end of the script
     if [[ -z "${nanolayer_location}" ]]; then
 
         if [ "$(uname -sm)" == "Linux x86_64" ] || [ "$(uname -sm)" == "Linux aarch64" ]; then
             tmp_dir=$(mktemp -d -t nanolayer-XXXXXXXXXX)
 
-            clean_up () {
+            clean_up() {
                 ARG=$?
                 rm -rf $tmp_dir
                 exit $ARG
             }
             trap clean_up EXIT
 
-            
-            if [ -x "/sbin/apk" ] ; then
+            if [ -x "/sbin/apk" ]; then
                 clib_type=musl
             else
                 clib_type=gnu
@@ -159,11 +156,10 @@ ensure_nanolayer() {
 
             # clean download will minimize leftover in case a downloaderlike wget or curl need to be installed
             clean_download https://github.com/devcontainers-contrib/cli/releases/download/$required_version/$tar_filename $tmp_dir/$tar_filename
-            
+
             tar xfzv $tmp_dir/$tar_filename -C "$tmp_dir"
             chmod a+x $tmp_dir/nanolayer
             nanolayer_location=$tmp_dir/nanolayer
-      
 
         else
             echo "No binaries compiled for non-x86-linux architectures yet: $(uname -m)"
@@ -175,4 +171,3 @@ ensure_nanolayer() {
     declare -g ${variable_name}=$nanolayer_location
 
 }
-
